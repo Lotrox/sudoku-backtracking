e183b2f06daa42a991bd0e75b868376540383b82
Daniel Martínez <dmc00024@red.ujaen.es>
2015-05-06 01:44:19 +0200

    Origen

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Lotrox
+ *
+ * Created on 6 de mayo de 2015, 1:43
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
